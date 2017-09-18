94795182ede127de6bfca0719d9974228e2d2540
jvaira <jvaira@uwyo.edu>
2017-09-17 21:36:05 -0600

    progress...?
    I have no idea where my program is getting these numbers, they're not in
    the files its reading. I probably have it set to reading them in as the
    wrong type or something stupid like that but it's annoying.

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ http://www.tutorialspoint.com/cplusplus/cpp_files_streams.htm
 http://en.cppreference.com/w/cpp/header
 http://www.cplusplus.com/reference/istream/istream/
 http://www.cplusplus.com/reference/cctype/isspace/
+https://stackoverflow.com/a/33097502 (visual studio likes it's files in a different place so I had to look this up because I couldn't remember where the program I use at home puts its)
--- lab-1/lab-1/lab-1.cpp
+++ lab-1/lab-1/lab-1.cpp
@@ -1,6 +1,5 @@
 // lab-1.cpp : Defines the entry point for the console application.
-//okay this lab is to count the number of numbers in a text file or something. We had this exact assignment back in 1030 so if I get really stuck I can go look in that book.
-//First time using visual studio. Why are there so many options. 
+//okay this lab is to count the number of numbers in a text file or something.
 
 #include "stdafx.h"
 #include <iostream>
@@ -20,6 +19,7 @@ int main()
 	ifstream infile;
 	//open file
 	infile.open(fileName);
+    //display message if opening file fails
 	if (infile.fail()) 
 		{
 			cout << "Couldn't open your file. Bye." << endl;
@@ -28,23 +28,36 @@ int main()
 		{
 		//int to hold number of numbers counted
 		int numberCount = 0;
+        //doubles to hold first and last two numbers
+        double numOne, numTwo, numThree, numFour;
+            //initialize them to zero so things don't point to whatever, just to be safe
+            numOne = numTwo = numThree = numFour = 0;
 			while (infile.get())
 			{
 				double x = infile.get();
-				if (isspace(x))
+				if (!isspace(x))
 				{
 					numberCount = numberCount + 1;
-					cout << "Counting...\n";
+					cout << infile.get() << endl;
+                    if (numberCount == 1)
+                    {
+                        numOne = x;
+                    }
+
 				}
+                //tell user when file is done being read and get out of this while loop, maybe not necessary,
+                // I can't remember what I was thinking exactly when I did this part but it works so I'll leave it
+                // for now
 				if (infile.eof())
 				{
 					cout << "End of file reached.\n";
 					break;
 				}
 			}
+        //tell the people what the results are
 		cout << "Number of numbers in file: " << numberCount << endl;
-		cout << "First two numbers: " << ", " << endl;
-		cout << "Last two numbers: " <<  ", " << endl;
+		cout << "First two numbers: " << numOne << ", " << numTwo << endl;
+		cout << "Last two numbers: " << numThree << ", " << numFour << endl;
 		}
 	//close the file when you're done with it
 	infile.close();
